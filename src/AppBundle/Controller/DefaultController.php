6f5a7c31df60c18fa7c6eb975de18637997b3b70
andreybolonin <andreybolonin1989@gmail.com>
2016-04-18 16:49:34 +0300

    added checkout actions

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,37 @@ class DefaultController extends Controller
 //            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
+
+    /**
+     * @Route("/tariff", name="tariff")
+     */
+    public function tariffAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/tariff.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/delivery", name="delivery")
+     */
+    public function deliveryAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/delivery.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+    
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function paymentAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/payment.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
 }
