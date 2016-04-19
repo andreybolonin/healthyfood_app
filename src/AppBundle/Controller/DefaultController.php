ed3a820781aff303a00e8fc40dccdf3b3318b04d
andreybolonin <andreybolonin1989@gmail.com>
2016-04-19 03:11:30 +0300

    fixed payment

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Lead;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Goutte\Client;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class DefaultController extends Controller
 {
@@ -42,6 +46,37 @@ class DefaultController extends Controller
             date_format(new \DateTime('+7 days'), 'Y-m-d') => $fmt->format(new \DateTime('+7 days')),
         ];
 
+
+
+        return $this->render('default/delivery.html.twig', [
+            'tariff' => $tariff,
+            'delivery' => $delivery,
+            'data' => $data,
+            'signature' => $signature
+        ]);
+    }
+
+    /**
+     * @Route("/payment/{tariff}", name="payment")
+     */
+    public function paymentAction(Request $request)
+    {
+        // save delivery data &
+        $em = $this->getDoctrine()->getManager();
+        $lead = new Lead();
+
+        $lead->setName($request->get('name'));
+        $lead->setSurname($request->get('surname'));
+        $lead->setAddress($request->get('address'));
+        $lead->setBuilding($request->get('building'));
+        $lead->setEmail($request->get('email'));
+        $lead->setPhone($request->get('phone'));
+        $lead->setTariff($request->get('tariff'));
+        $lead->setDeliveryDate($request->get('delivery_date'));
+
+        $em->persist($lead);
+        $em->flush();
+
         $private_key = 'Lfj88TBWC2wCc9T8vCm2ZunA5qBKkR8SZAKcN0h0';
         $json_string = [
             'version' => 3,
@@ -54,27 +89,98 @@ class DefaultController extends Controller
         ];
 
         $data = base64_encode(json_encode($json_string));
-
         $signature = base64_encode(sha1($private_key.$data.$private_key, 1));
 
-        return $this->render('default/delivery.html.twig', [
-            'tariff' => $tariff,
-            'delivery' => $delivery,
-            'data' => $data,
-            'signature' => $signature
-        ]);
-    }
+//        $subRequest = Request::create('https://www.liqpay.com/api/3/checkout', 'POST', ['data' => $data, 'signature' => $signature]);
+//        return new RedirectResponse($subRequest);
+//        return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
 
-    /**
-     * @Route("/payment", name="payment")
-     */
-    public function paymentAction(Request $request)
-    {
-        // save delivery data &
+//        $client = new Client();
+//        $crawler = $client->request('POST', 'https://www.liqpay.com/api/3/checkout', [
+//            'data' => $data,
+//            'signature' => $signature
+//        ]);
+
+        $this->sendHttpRequest();
+
+       $this->redirect_post('https://www.liqpay.com/api/3/checkout', ['data' => $data, 'signature' => $signature]);
+
+//        return $this->redirect();
+
+//        return $this->redirectToRoute('https://www.liqpay.com/api/3/checkout', ['data' => $data,
+//            'signature' => $signature], 301);
+        
+//        return new RedirectResponse('https://www.liqpay.com/api/3/checkout', 302, )
+
         // redirect post to https://www.liqpay.com/api/3/checkout
         
-        return $this->render('default/payment.html.twig', [
+//        return $this->render('default/payment.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+//        ]);
+    }
+
+
+    /**
+     * Redirect with POST data.
+     *
+     * @param string $url URL.
+     * @param array $data
+     * @param array $headers Optional. Extra headers to send.
+     * @throws \Exception
+     * @internal param array $post_data POST data. Example: array('foo' => 'var', 'id' => 123)
+     */
+    public function redirect_post($url, array $data, array $headers = null) {
+        $params = array(
+            'http' => array(
+                'method' => 'POST',
+                'header'  => 'Content-type: multipart/form-data',
+                'content' => http_build_query($data)
+            )
+        );
+        if (!is_null($headers)) {
+            $params['http']['header'] = '';
+            foreach ($headers as $k => $v) {
+                $params['http']['header'] .= "$k: $v\n";
+            }
+        }
+        $ctx = stream_context_create($params);
+        $fp = fopen($url, 'rb', false, $ctx);
+        if ($fp) {
+            echo stream_get_contents($fp);
+            die();
+        } else {
+            // Error
+            throw new \Exception("Error loading '$url', $php_errormsg");
+        }
+    }
+
+    function do_post_request($url, $data, $optional_headers = null)
+    {
+        $params = array('http' => array(
+            'method' => 'POST',
+            'content' => $data
+        ));
+        if ($optional_headers !== null) {
+            $params['http']['header'] = $optional_headers;
+        }
+        $ctx = stream_context_create($params);
+        $fp = @fopen($url, 'rb', false, $ctx);
+        if (!$fp) {
+            throw new \Exception("Problem with $url");
+        }
+        $response = @stream_get_contents($fp);
+        if ($response === false) {
+            throw new \Exception("Problem reading data from $url");
+        }
+        return $response;
+    }
+
+    private function sendHttpRequest($host, $path, $query, $port=80){
+        header("POST $path HTTP/1.1\r\n" );
+        header("Host: $host\r\n" );
+        header("Content-type: application/x-www-form-urlencoded\r\n" );
+        header("Content-length: " . strlen($query) . "\r\n" );
+        header("Connection: close\r\n\r\n" );
+        header($query);
     }
 }
---
+++ src/AppBundle/Entity/Lead.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lead.
+ *
+ * @ORM\Table(name="lead")
+ * @ORM\Entity()
+ */
+class Lead extends Base
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $surname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $building;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $deliveryDate;
+
+    /**
+     * @return mixed
+     */
+    public function getDeliveryDate()
+    {
+        return $this->deliveryDate;
+    }
+
+    /**
+     * @param mixed $deliveryDate
+     */
+    public function setDeliveryDate($deliveryDate)
+    {
+        $this->deliveryDate = $deliveryDate;
+    }
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $tariff;
+
+    /**
+     * @return mixed
+     */
+    public function getTariff()
+    {
+        return $this->tariff;
+    }
+
+    /**
+     * @param mixed $tariff
+     */
+    public function setTariff($tariff)
+    {
+        $this->tariff = $tariff;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param mixed $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBuilding()
+    {
+        return $this->building;
+    }
+
+    /**
+     * @param mixed $building
+     */
+    public function setBuilding($building)
+    {
+        $this->building = $building;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+}
